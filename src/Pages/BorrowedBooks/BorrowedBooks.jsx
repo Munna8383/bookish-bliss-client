0f6b1f8a4f54fd50a494ee5e67bbcded278cc8fd
Munna8383 <jahedmunna612@gmail.com>
2024-05-13 19:58:30 +0600

    book return

--- src/Pages/BorrowedBooks/BorrowedBooks.jsx
+++ src/Pages/BorrowedBooks/BorrowedBooks.jsx
@@ -15,10 +15,31 @@ const BorrowedBooks = () => {
         setData(res.data)
     })
    },[axiosSecure,user.email])
+
+
+
+   const handleReturn=id=>{
+
+    console.log(id)
+
+    axiosSecure.patch(`/returnBook/${id}`)
+    .then(res=>{
+        if(res.data.modifiedCount==1){
+
+            const filtered = data.filter(item=>item._id!==id)
+
+            setData(filtered)
+        }
+    })
+
+
+   }
    
    if(data.length==0){
     return <div className="flex justify-center mt-10 text-2xl font-bold mb-20">No Book Found! Please Borrow Book</div>
    }
+
+
     return (
         <div className="mt-10 w-11/12 mx-auto">
 
@@ -40,7 +61,7 @@ const BorrowedBooks = () => {
                           <h1>Borrow Date :<span className="font-semibold"> {item?.borrowDate}</span></h1>
                           <h1>Return Date :<span className="font-semibold"> {item?.returnDate}</span></h1>
                           <h1>Category:<span className="font-semibold"> {item?.category}</span></h1>
-                          <button className="btn mt-3 bg-[#c9c6ac] text-black font-bold w-full">Return</button>
+                          <button onClick={()=>handleReturn(item._id)} className="btn mt-3 bg-[#c9c6ac] text-black font-bold w-full">Return</button>
                       </div>
                       
                      
